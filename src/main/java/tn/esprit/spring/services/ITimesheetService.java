d718aae35125df04715ce641670cab17f54f97fe
nadhem salaani <salaani.nadhem@esprit.tn>
2021-11-15 20:35:17 +0100

    removed spaces

--- src/main/java/tn/esprit/spring/services/ITimesheetService.java
+++ src/main/java/tn/esprit/spring/services/ITimesheetService.java
@@ -6,9 +6,7 @@ import java.util.List;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Mission;
 
-
 public interface ITimesheetService {	
-	
 	public int ajouterMission(Mission mission);
 	public void affecterMissionADepartement(int missionId, int depId);
 	public void ajouterTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin);
